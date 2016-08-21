02d853efa9226a4ffc0e81e77819432f87ebebaf
gmkimma <gmkimma@yahoo.com>
2016-08-20 21:02:12 -0700

    Add GridView with Movie posters

--- app/src/main/java/com/gregkimma/popularmovies/MainActivity.java
+++ app/src/main/java/com/gregkimma/popularmovies/MainActivity.java
@@ -1,7 +1,15 @@
 package com.gregkimma.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar,
+                R.drawable.interstellar, R.drawable.interstellar
+        };
     }
 }
